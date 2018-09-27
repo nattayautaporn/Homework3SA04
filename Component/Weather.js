f5fdf7e35ba94263d4d78b721b757dda2ba9cc0e
nattayautaporn <parn_6740@hotmail.com>
2018-09-27 10:46:17 +0700

    frist commit

---
+++ Component/Weather.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class Weather extends React.Component {
+    doIt = () => {
+        console.log("Hello from console")
+        }
+    render() {
+    return (
+    <View style={styles.container}>
+    <Text>{this.props.zipCode}</Text>
+    </View>
+    );
+    }
+   }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
