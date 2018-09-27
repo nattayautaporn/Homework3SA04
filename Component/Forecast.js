ea3bd0c7339263d1b553d53b0376ed106e3d7fdf
nattayautaporn <parn_6740@hotmail.com>
2018-09-27 11:55:41 +0700

    change w and f

--- Component/Forecast.js
+++ Component/Forecast.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { While } from 'ansi-colors';
+import { StyleSheet,Text, View } from 'react-native';
+import { white } from 'ansi-colors';
 
 export default class Forecast extends React.Component {
     render() {
-    return (
-    <View>
-    <Text>{this.props.main}</Text>
-    <Text>{this.props.description}</Text>
-    <Text>{this.props.temp}</Text>
-    <Text>°C</Text>
-    </View>
-    );
+        return (
+            
+            <View style={{flex: 1, flexDirection: 'column',alignItems: 'center'}}>
+                <View style={{alignSelf: 'stretch', height: 250, backgroundColor: 'black', opacity: .5}} >             
+                    <Text style= {styles.text1}>{"\n"}Zip code is {this.props.zipcode}.</Text> 
+                    <Text style= {styles.text2}>{"\n"}{this.props.main}</Text>
+                    <Text style= {styles.text3}>{"\n"}{this.props.description}</Text>
+                    <Text style= {styles.text4}>{"\n"}{this.props.temp} °C</Text>
+                </View>           
+            </View>
+        );
     }
-   }
-   
-   
+}
+const styles = StyleSheet.create({
+    text1: {textAlign: 'center' , fontSize: 20 , color:'white'},
+    text2: {textAlign: 'center' , fontSize: 30 , color:'white'},
+    text3: {textAlign: 'center' , fontSize: 18 , color:'white'},
+    text4: {textAlign: 'center' , fontSize: 28 , color:'white'},
+  });
