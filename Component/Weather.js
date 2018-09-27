1382bce7747b254a2d145336692563592c6fade2
nattayautaporn <parn_6740@hotmail.com>
2018-09-27 11:55:02 +0700

    change weather

--- Component/Weather.js
+++ Component/Weather.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {styleSheet, Text,View,ImageBackground}from'react-native';
 
 export default class Weather extends React.Component {
-    doIt = () => {
-        console.log("Hello from console")
-        }
-    render() {
-    return (
-    <View style={styles.container}>
-    <Text>{this.props.zipCode}</Text>
-    </View>
-    );
+  export default class Weather extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+    forecast: {
+    main: '-', description: '-', temp: 0
     }
-   }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  }
+}
+     
+render() {
+  return (
+  <View style={styles.container}>
+  <ImageBackground source={require('../bg.jpeg')} style={styles.backdrop}>
+  <Text>Zip code is {this.props.zipCode}.</Text>
+  <Forecast {...this.state.forecast} />
+  </ImageBackground>
+  </View>
+  );
+  }
+ }
+ const styles = StyleSheet.create({
+  container: { paddingTop: 25 },
+  backdrop: { width: '100%', height: '100%'},
+ });
